e48fb1436c59539095764a8b58716e823dd1ac57
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2019-11-19 09:49:01 -0800

    fleshed out how a note looks without description

--- src/center/Note.js
+++ src/center/Note.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
 export default function Note(props) {
-  
+  return (
+    <div className='note'>
+      <h3>{props.noteTitle}</h3>
+      {props.dateModified}
+      <button>Delete</button>
+    </div>
+  )
 }
